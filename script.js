2c114d656ec6de5201d4baf1efcc44d66f6b8047
Victormarks10 <aaron.victormarks10@gmail.com>
2023-08-01 15:31:40 -0300

    reponsividade

--- script.js
+++
@@ -1,8 +0,0 @@
-const menu = document.querySelector('.menu')
-const Navmenu = document.querySelector('.nav-menu')
-
-
-menu.addEventListener('click', ()=>{
-menu.classList.toggle('active')
-Navmenu.classList.toggle('active')
-})
